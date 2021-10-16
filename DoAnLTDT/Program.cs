bb6c25ab1f1bc488b453223987aff40e4a6f8811
haiphan-urbn8 <hai.phan@urbn8.com>
2021-10-16 21:27:47 +0700

    Set up structure va lam cau 1: kiem tra do thi trong

--- DoAnLTDT/Program.cs
+++ DoAnLTDT/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DoAnLTDT
 {
@@ -16,6 +17,19 @@ namespace DoAnLTDT
         {
             public int numberOfVertexs;
             public int[,] adjacencyList;
+        }
+
+        public enum GrapTypes
+        {
+            EMTPY_GRAPH,
+            CYCLE_GRAPH,
+            BUTTERFLY_GRAPH,
+            MOTH_GRAPH,
+            STAR_GRAPH,
+            WHEEL_GRAPH,
+            BARBELL_GRAPH,
+            FRIENDSHIP_GRAPH,
+            K_PARTITE_GRAPH
         }
 
         public static string GetFilePath(string fileName)
@@ -118,6 +132,165 @@ namespace DoAnLTDT
             }
         }
 
+        public static int CountEdges(AdjacencyMatrix g)
+        {
+            int totalEdges = 0;
+            List<int[]> countedIndexes = new List<int[]>();
+            int i, j;
+            for (i = 0; i < g.numberOfVertexes; ++i)
+            {
+                for (j = 0; j < g.numberOfVertexes; ++j)
+                {
+                    // voi moi dinh (i,j)
+                    // kiem tra xem cap dinh da duoc dem chua
+                    // chi can [i,j] hoac [j,i] da ton tai trong mang countedIndexes nghia la cap dinh (i,j) da duoc dem
+                    bool counted = countedIndexes.Where(item => (item[0] == i && item[1] == j) || item[0] == j && item[1] == i).ToArray().Length > 0;
+
+                    int edges = g.adjacencyMatrix[i, j];
+
+                    // neu cap (i,j) chua duoc dem va giua chung co canh noi thi them vao danh sach dinh da dem
+                    if (!counted && edges > 0)
+                    {
+                        totalEdges += edges;
+                        countedIndexes.Add(new int[] { i, j });
+                    }
+                }
+            }
+            return totalEdges;
+        }
+
+        public static void CheckForEmptyGraph(AdjacencyMatrix am, Dictionary<GrapTypes, dynamic> gt)
+        {
+            int numberOfEdges = CountEdges(am);
+            if (numberOfEdges == 0)
+            {
+                gt[GrapTypes.EMTPY_GRAPH] = am.numberOfVertexes;
+            }
+        }
+
+        public static string FormatKPartiteToString(List<List<int>> p)
+        {
+            string result = "";
+            foreach (List<int> part in p)
+            {
+                string temp = "";
+                for (int i = 0; i < part.Count; ++i)
+                {
+                    if (i == 0)
+                    {
+                        temp += "{";
+                    }
+                    temp += $"{i}";
+                    if (i < part.Count - 1)
+                    {
+                        temp += ",";
+                    }
+                    if (i == part.Count - 1)
+                    {
+                        temp += "}";
+                    }
+                }
+                result += $"{temp} ";
+            }
+            return result;
+        }
+
+        public static Dictionary<GrapTypes, dynamic> ConstrucDefaultTypeMapping()
+        {
+            Dictionary<GrapTypes, dynamic> graphTypeMapping = new Dictionary<GrapTypes, dynamic>
+            {
+                [GrapTypes.EMTPY_GRAPH] = 0,
+                [GrapTypes.CYCLE_GRAPH] = 0,
+                [GrapTypes.BUTTERFLY_GRAPH] = 0,
+                [GrapTypes.MOTH_GRAPH] = 0,
+                [GrapTypes.STAR_GRAPH] = 0,
+                [GrapTypes.WHEEL_GRAPH] = 0,
+                [GrapTypes.BARBELL_GRAPH] = 0,
+                [GrapTypes.FRIENDSHIP_GRAPH] = 0,
+                [GrapTypes.K_PARTITE_GRAPH] = new List<List<int>> { }
+            };
+
+            return graphTypeMapping;
+        }
+
+        public static void PrintResult(Dictionary<GrapTypes, dynamic> graphTypeMapping)
+        {
+            foreach (GrapTypes key in graphTypeMapping.Keys)
+            {
+                switch (key)
+                {
+                    case GrapTypes.EMTPY_GRAPH:
+                        int k = graphTypeMapping[GrapTypes.EMTPY_GRAPH];
+                        bool isEmptyGraph = k > 0;
+                        string result = isEmptyGraph ? $"k = {k}" : "Khong";
+                        Console.WriteLine($"Do thi trong: {result}");
+                        break;
+
+                    case GrapTypes.CYCLE_GRAPH:
+                        k = graphTypeMapping[GrapTypes.CYCLE_GRAPH];
+                        bool isCycleGraph = k > 0;
+                        result = isCycleGraph ? $"k = {k}" : "Khong";
+                        Console.WriteLine($"Do thi vong: {result}");
+                        break;
+
+                    case GrapTypes.BUTTERFLY_GRAPH:
+                        bool isButterflyGraph = graphTypeMapping[GrapTypes.BUTTERFLY_GRAPH] > 0;
+                        result = isButterflyGraph ? $"Co" : "Khong";
+                        Console.WriteLine($"Do thi hinh con buom: {result}");
+                        break;
+
+                    case GrapTypes.MOTH_GRAPH:
+                        bool isMothGraph = graphTypeMapping[GrapTypes.MOTH_GRAPH] > 0;
+                        result = isMothGraph ? $"Co" : "Khong";
+                        Console.WriteLine($"Do thi hinh con ngai: {result}");
+                        break;
+
+                    case GrapTypes.STAR_GRAPH:
+                        k = graphTypeMapping[GrapTypes.STAR_GRAPH];
+                        bool isStarGraph = k > 0;
+                        result = isStarGraph ? $"k = {k}" : "Khong";
+                        Console.WriteLine($"Do thi hinh sao: {result}");
+                        break;
+
+                    case GrapTypes.WHEEL_GRAPH:
+                        k = graphTypeMapping[GrapTypes.WHEEL_GRAPH];
+                        bool isWheelGraph = k > 0;
+                        result = isWheelGraph ? $"k = {k}" : "Khong";
+                        Console.WriteLine($"Do thi hinh banh xe: {result}");
+                        break;
+
+                    case GrapTypes.BARBELL_GRAPH:
+                        k = graphTypeMapping[GrapTypes.BARBELL_GRAPH];
+                        bool isBarbellGraph = k > 0;
+                        result = isBarbellGraph ? $"k = {k}" : "Khong";
+                        Console.WriteLine($"Do thi Barbell: {result}");
+                        break;
+
+                    case GrapTypes.FRIENDSHIP_GRAPH:
+                        k = graphTypeMapping[GrapTypes.FRIENDSHIP_GRAPH];
+                        bool isFriendshipGraph = k > 0;
+                        result = isFriendshipGraph ? $"k = {k}" : "Khong";
+                        Console.WriteLine($"Do thi tinh ban: {result}");
+                        break;
+
+                    case GrapTypes.K_PARTITE_GRAPH:
+                        List<List<int>> p = graphTypeMapping[GrapTypes.K_PARTITE_GRAPH];
+                        bool isKPartiteGraph = p.Count > 0;
+                        if (!isKPartiteGraph)
+                        {
+                            Console.WriteLine($"Do thi k phan: Khong");
+                            break;
+                        }
+                        result = $"k = {p.Count} {FormatKPartiteToString(p)}";
+                        Console.WriteLine($"Do thi k phan: {result}");
+                        break;
+
+                    default:
+                        Console.WriteLine($"Do thi khong duoc ho tro!");
+                        break;
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
@@ -126,6 +299,8 @@ namespace DoAnLTDT
             AdjacencyList[] ALs = ReadMultiAL(filePath);
 
             // In danh sach AL va them vao AMs
+            Console.WriteLine("In danh sach ke(AL) va ma tran ke(AM)");
+            Console.WriteLine();
             AdjacencyMatrix[] AMs = new AdjacencyMatrix[ALs.Length];
             for (int i = 0; i < ALs.Length; ++i)
             {
@@ -135,6 +310,17 @@ namespace DoAnLTDT
                 Print2DArray(ALs[i].adjacencyList);
                 Console.WriteLine("AM");
                 Print2DArray(am);
+                Console.WriteLine("-----------------------");
+            }
+
+            // kiem tra loai do thi cho tung AM
+            Console.WriteLine("kiem tra loai do thi cho tung ma tran ke");
+            Console.WriteLine();
+            foreach (AdjacencyMatrix am in AMs)
+            {
+                Dictionary<GrapTypes, dynamic> graphTypeMapping = ConstrucDefaultTypeMapping();
+                CheckForEmptyGraph(am, graphTypeMapping);
+                PrintResult(graphTypeMapping);
                 Console.WriteLine("-----------------------");
             }
         }
